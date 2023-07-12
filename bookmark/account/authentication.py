25d96c9f6fe8e2d29247993bcde4a676923f78e7
maksim <sustavovm@yandex.ru>
2023-07-12 19:46:36 +1100

    Finished 5 and 6 paragraphs

--- bookmark/account/authentication.py
+++ bookmark/account/authentication.py
@@ -1,4 +1,13 @@
 from django.contrib.auth.models import User
+from .models import Profile
+
+
+def create_profile(backend, user, *args, **kwargs):
+    """
+    Создать профиль пользователя для социальной аутентификации
+    """
+    Profile.objects.get_or_create(user=user)
+
 
 
 class EmailAuthBackend:
--- bookmark/bookmark/settings.py
+++ bookmark/bookmark/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-b*%13hs@rgvq^-cmjelz_(o-v^zq(kb6vxx8m$!36q1*espnbl
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mysite.com','localhost', '127.0.0.1']
 
 
 # Application definition
@@ -27,6 +27,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'social_django',
+    'django_extensions',
+    'images.apps.ImagesConfig',
+    'easy_thumbnails',
 ]
 
 MIDDLEWARE = [
@@ -44,6 +48,8 @@ ROOT_URLCONF = 'bookmark.urls'
 AUTHENTICATION_BACKENDS = [
             'django.contrib.auth.backends.ModelBackend',
             'account.authentication.EmailAuthBackend',
+            'social_core.backends.google.GoogleOAuth2',
+            "social_core.backends.vk.VKOAuth2"
 ]
 
 TEMPLATES = [
@@ -125,6 +131,33 @@ LOGOUT_URL = 'logout'
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
+SOCIAL_AUTH_PIPELINE = [
+    'social_core.pipeline.social_auth.social_details',
+    'social_core.pipeline.social_auth.social_uid',
+    'social_core.pipeline.social_auth.auth_allowed',
+    'social_core.pipeline.social_auth.social_user',
+    'social_core.pipeline.user.get_username',
+    'social_core.pipeline.user.create_user',
+    'account.authentication.create_profile',
+    'social_core.pipeline.social_auth.associate_user',
+    'social_core.pipeline.social_auth.load_extra_data',
+    'social_core.pipeline.user.user_details',
+]
+
+SOCIAL_AUTH_URL_NAMESPACE = 'social'
+
+SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = '1025136212867-j1p42ur8ucks6iemr9nivm3eb30m8dle.apps.googleusercontent.com'
+
+SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = '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'
+
+SOCIAL_AUTH_VK_OAUTH2_KEY = '51675716'
+
+SOCIAL_AUTH_VK_OAUTH2_SECRET = 'fio8DH5dcpizAu7wNQm3'
+
+SOCIAL_AUTH_VK_OAUTH2_EXTRA_DATA = ['first_name', 'last_name', 'email']
+
 MEDIA_URL = 'media/'
 
 MEDIA_ROOT = BASE_DIR / 'media'
+
+
